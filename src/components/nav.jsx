c7190d5ca88a37b9834dfc7eb8ffe66475d7f90e
Roshan-ops <60165267+Roshan-ops@users.noreply.github.com>
2023-01-30 20:32:32 +0545

    update

---
+++ src/components/nav.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Container, Navbar } from "react-bootstrap";
+
+function Nav() {
+  return (
+    <>
+    <Navbar expanded='lg' varient='light'>
+    <Container className="nav-container">
+
+    </Container>
+    </Navbar>
+
+    </>
+  )
+}
+
+export default Nav
