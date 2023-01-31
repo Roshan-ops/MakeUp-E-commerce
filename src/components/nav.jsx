3136a11065d6308e2a1e3245f83a5a23fb22f1ee
Roshan-ops <60165267+Roshan-ops@users.noreply.github.com>
2023-01-31 17:57:15 +0545

    update

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -5,7 +5,9 @@ function Nav() {
   return (
     <>
     <Navbar expanded='lg' varient='light'>
-    <Container className="nav-container">
+    <Container className="nav-container col1 " varient='black' expanded='lg'>
+      <h1> L'YON</h1>
+
 
     </Container>
     </Navbar>
