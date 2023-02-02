b67b033ae528625cc2ae6311714b829647132020
Roshan-ops <60165267+Roshan-ops@users.noreply.github.com>
2023-02-03 00:15:01 +0545

    update

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -7,7 +7,11 @@ function Nav() {
     <Navbar expanded='lg' varient='light'>
     <Container className="nav-container col1 " varient='black' expanded='lg'>
       <h1> L'YON</h1>
+<p>hello sanchi ho sathi</p>
+<div className='Hello' varient='primary'>
+  <p>Khana khako</p>
 
+</div>
 
     </Container>
     </Navbar>
