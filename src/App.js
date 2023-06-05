7808ce64234c2b63a38da360f06f51b607331718
Roshan-ops <60165267+Roshan-ops@users.noreply.github.com>
2023-06-05 17:54:27 +0545

    Changes

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import './components/nav.jsx';
 import './App.css';
 import Nav from './components/nav.jsx';
 import Login from './components/login.jsx';
-
+import Banner from './components/banner.jsx';
 
 function App() {
   return (
     <>
    <Nav/>
    <Login/>
+   <Banner/>
    </>
 
   );
---
+++ src/components/banner.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+
+function banner() {
+  return (
+    <div className='banter-container'>
+        <div>
+            <h4> Beauty products that really <br></br>Works</h4>
+        </div>
+        <p> Our formulations have proven efficacy, contain organic <br></br> ingredients only and are 100% cruelty free.</p>
+        <div className='button'>
+<button>Skin Care</button>
+<div className='button1'><button>Shop All</button>
+</div>
+</div>
+
+    </div>
+  )
+}
+
+export default banner
